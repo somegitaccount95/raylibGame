c75bdb65c0d3525b68c505be14f4436a8368e224
Max R <maxrepkar@gmail.com>
2026-07-13 18:56:01 -0500

    first commit

---
+++ src/main.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <raylib.h>
+
+int main(int argc, char const *argv[])
+{
+    (void) argc;
+    (void) argv;
+
+    SetConfigFlags(FLAG_FULLSCREEN_MODE);
+
+    InitWindow(1920, 1080, "Game");
+
+    SetTargetFPS(60);
+
+    while (!WindowShouldClose())
+    {
+
+        BeginDrawing();
+
+            ClearBackground(Color({0, 0, 0, 1}));
+            DrawText(TextFormat("FPS: %i", GetFPS()), 20, 5, 50, WHITE);
+
+        EndDrawing();
+    }
+    
+    CloseWindow();
+
+    return 0;
+}
